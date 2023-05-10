dba954a3e82a6dbb6d99994ef4b958fa1fea2ecb
Ubuntu <ubuntu@ip-172-31-43-168.eu-west-1.compute.internal>
2023-05-10 07:51:32 +0000

    3 dernières corrections PEP 8 pour file_test.py

--- file_test.py
+++ file_test.py
@@ -1,8 +1,8 @@
-#import pytest
+# import pytest
 
 
 def test_calc_addition():
-  # Fonction test du résultat de 2+4
+    # Fonction test du résultat de 2+4
     output = 2+4
     assert output == 6
 
@@ -21,5 +21,5 @@ def test_calc_multiply():
 
 def test_coucou():
     # Fonction test si la résultat renvoie 'hello'
-    output='hello'
+    output = 'hello'
     assert output == 'hello'
